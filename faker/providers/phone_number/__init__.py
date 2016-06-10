0643c7bee60a950f36e86050d23dc3458454c1c1
Stephen Ross II <sross@mediweightloss.com>
2016-06-10 12:02:51 -0400

    Add option to manually specify a phone number format

--- faker/providers/phone_number/__init__.py
+++ faker/providers/phone_number/__init__.py
@@ -7,5 +7,14 @@ class Provider(BaseProvider):
     formats = ('###-###-###',)
 
     @classmethod
-    def phone_number(cls):
+    def phone_number(cls, format=None):
+        """
+        Creates a random phone number from the current locale
+
+        :param format: optional format string to force a specific output format
+        :example '1-800-867-5309'
+        :return str
+        """
+	if format is not None:
+            return cls.numerify(format)
         return cls.numerify(cls.random_element(cls.formats))
